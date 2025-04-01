7b2c5376f10095aae4ea06d5306200b5a42e4629
unbounddev <unbounddev@git>
2025-04-01 09:43:31 -0700

    Configure PrimeVue and get rid of default files and styling

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
+import { Noir } from './theme'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Noir,
+  },
+})
 
 app.mount('#app')
